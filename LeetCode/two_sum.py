f68e44a7538cad4c884e583edf7d3d25877e2cb5
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2026-03-03 06:43:45 +0300

    Add Hashmap Solution to make it O(N^2) -> O(N).

--- LeetCode/two_sum.py
+++ LeetCode/two_sum.py
@@ -1,7 +1,13 @@
-class Solution(object):
+# LeetCode '
 
+class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        numbers = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in numbers:
+                return [numbers[complement], i]
+            else:
+                numbers[num] = i
+        
