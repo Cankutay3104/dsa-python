c9ef3c178acc22fe416516ed49cd596b33acd9cc
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2026-06-27 12:34:08 +0300

    Add LeetCode 11. Container With Most Water Solution

---
+++ LeetCode/container_with_most_water.py
@@ -0,0 +1,19 @@
+# LeetCode "11. Container With Most Water" Solution
+
+class Solution(object):
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        
+        while left != right:
+            current_area = (right - left) * min(height[left], height[right])
+            if current_area > max_area:
+                max_area = current_area
+                
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+            
+        return max_area
