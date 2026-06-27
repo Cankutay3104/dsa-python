1523510350122a983b19e201dc669a429ced3ecc
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2026-06-27 14:00:09 +0300

    Add LeetCode 643. Maximum Average Subarray I Solution

---
+++ LeetCode/maximum_average_subarray_I.py
@@ -0,0 +1,16 @@
+# LeetCode "643. Maximum Average Subarray I" Solution
+
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        current_sum = 0
+        for i in range(k):
+            current_sum += nums[i]
+        
+        max_sum = current_sum
+
+        for i in range(k, len(nums)):
+            current_sum += nums[i] - nums[i-k]
+            if current_sum > max_sum:
+                max_sum = current_sum
+
+        return float(max_sum)/k
