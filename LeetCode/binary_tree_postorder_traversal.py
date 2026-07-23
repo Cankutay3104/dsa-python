df4501911d8ec8bd050e3a2b93aeef0b6c272dde
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2026-07-23 08:14:03 +0300

    Add LeetCode 145. Binary Tree Postorder Traversal Solution

---
+++ LeetCode/binary_tree_postorder_traversal.py
@@ -0,0 +1,22 @@
+# LeetCode "145. Binary Tree Postorder Traversal" Solution
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        result = []
+
+        def dfs(node):
+            if not node:
+                return
+            
+            dfs(node.left)
+            dfs(node.right)
+            result.append(node.val)
+
+        dfs(root)
+        return result
