ea679334e39985831970bb63536c3f8105836c5b
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2026-08-14 08:41:11 +0300

    Add LeetCode 79. Word Search Solution

---
+++ LeetCode/word_search.py
@@ -0,0 +1,27 @@
+# LeetCode "79. Word Search" Solution
+
+class Solution(object):
+    def exist(self, board, word):
+        rows, cols = len(board), len(board[0])
+
+        def dfs(row, col, index):
+            if index == len(word):
+                return True
+
+            if row < 0 or row >= rows or col < 0 or col >= cols or board[row][col] != word[index]:
+                return False
+
+            temp = board[row][col]
+            board[row][col] = '-'
+
+            correct = dfs(row-1, col, index+1) or dfs(row+1, col, index+1) or dfs(row, col-1, index+1) or dfs(row, col+1, index+1)
+
+            board[row][col] = temp
+
+            return correct
+
+        for row in range(rows):
+            for col in range(cols):
+                if dfs(row, col, 0):
+                    return True
+        return False
